ad646f5f56d9a070c34cc3b0b5757f1fbbd25001
Connor-OS <coshaughnessy@companieshouse.gov.uk>
2024-10-08 21:19:57 +0100

    improve carosel behaviour

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,19 @@ const courseList = ["Language Course", "University", "Private school", "Distance
 const cityList = ["Newcastle-Upon-Tyne", "York", "Leeds", "Manchester", "Bristol"];
 const ageList = ["16+", "7-16"];
 
+const carousel_elements = [{name: "Newcastle", desc: "Newcastle is a captivating city in northeastern England that beckons...", image: "Newcastle.png"},
+    {name: "Bristol", desc: "Bristol, a captivating city in southwestern England, is renowned...", image: "Bristol.png"},
+    {name: "Manchester", desc: "Manchester, located in the heart of England, is a vibrant...", image: "Manchester.png"}
+]
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' ,
                                     courseList: courseList,
                                     cityList: cityList,
-                                    ageList: ageList});
+                                    ageList: ageList,
+                                    carousel_elements: carousel_elements});
 });
 
 module.exports = router;
