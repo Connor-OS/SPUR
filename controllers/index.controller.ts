af6c09c0a5804ed6b9ffb45645d3e8213aaabe1a
Connor-OS <coshaughnessy1@gmail.com>
2025-01-14 23:24:11 +0000

    add what is waiting for you section of index

--- controllers/index.controller.ts
+++ controllers/index.controller.ts
@@ -1,18 +1,21 @@
 import {City, School, schoolTypeEnum} from "../model/dataModel";
+import reviews from "../model/testData/reviews.json";
 
 const languageList = ["Learn English", "Learn Spanish", "Learn French", "Learn German"];
-const ageList = ["16+", "7-16"]; //TODO: make these global variables set on initialisation
+const ageList = ["16+", "7-16"]; //TODO: make these global variables set on initialisation or store in db
 
 /* GET home page. */
 export const get = async (req, res, next) => {
     const cities = await City.find();
+    // const reviews = require("../model/testData/reviews.json");
 
     res.render('index', {
         frontPage: "frontpage",
         languageList: languageList,
         cityList: cities.map(city => city.name),
         ageList: ageList,
-        carousel_elements: cities
+        carousel_elements: cities,
+        reviews: reviews
     });
 };
 
---
+++ public/javascripts/page_utils.js
@@ -0,0 +1,18 @@
+function what_is_waiting_select(element, id_num) {
+
+    Array.from(
+        document.getElementById("what-is-waiting-selection").children
+    ).forEach((item) => {
+        item.classList.remove("selected")
+    });
+
+    element.classList.add("selected");
+
+    Array.from(
+        document.getElementById("what-is-waiting-info").children
+        ).forEach((item) => {
+        item.classList.add("hidden");
+    });
+
+    document.getElementById("what-is-waiting-info" + id_num).classList.remove("hidden");
+}
