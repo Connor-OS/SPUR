b86cfeb315f11f83548f0ccfafa3457096091ebc
Connor-OS <coshaughnessy1@gmail.com>
2025-02-12 18:23:21 +0000

    imporove your details form

--- public/javascripts/your-offer.js
+++ public/javascripts/your-offer.js
@@ -1,73 +1,138 @@
-const costInput = document.getElementById('cost_per_week');
 const courseInput = document.getElementById('courseDropdown');
+const courseCost = document.getElementById('cost_course');
 const courseDateInput = document.getElementById('course_dates');
 
-const total = document.getElementById('course-total')
+const accommodationInput = document.getElementById('accommodationDropdown');
+const accommodationCost = document.getElementById('cost_accommodation');
+const accommodationDateInput = document.getElementById('accommodation_dates');
+
+
+const total = document.getElementById('course-total');
 
 function syncCourseSelection(element) {
     // ensure data is synced between the course selected at the top of the page and
     // the details form at the bottom
     Array.from(
         element.parentElement.getElementsByClassName("course")
-    ).forEach((item) => deSelectCourse(item));
+    ).forEach((item) => deSelectElement(item));
 
-    selectCourse(element)
+    selectElement(element);
 
-    courseInput.value = element.querySelector('h3').innerText;
-    costInput.value = element.dataset.cost
+    courseCost.value = element.dataset.cost;
 
-    document.getElementById("course_name").innerHTML =
-        element.querySelector('h3').innerText;
-
-    document.getElementById("course_size").innerHTML =
-        element.querySelector('li:nth-of-type(1) p').innerText;
-
-    document.getElementById("course_schedule").innerHTML =
-        element.querySelector('li:nth-of-type(2) p').innerText;
-
-    calculate_total()
+    populateDetails(element, "course");
+    calculate_total();
 }
 
+function syncAccommodationSelection(element) {
+    // ensure data is synced between the accommodation selected at the top of the page and
+    // the details form at the bottom
+    Array.from(
+        element.parentElement.getElementsByClassName("accommodation")
+    ).forEach((item) => deSelectElement(item));
 
-function deSelectCourse(course) {
-    course.classList.remove("selected");
-    const button = course.children[1];
+    courseCost.value = element.dataset.cost
+
+    selectElement(element);
+
+    populateDetails(element, "accommodation");
+
+    accommodationCost.value = element.dataset.cost
+
+    const extraCosts = element.querySelectorAll('input[data-cost]:checked');
+    
+    Array.from(extraCosts).forEach((item) => accommodationCost.value = Number(accommodationCost.value) + Number(item.dataset.cost))
+    calculate_total();
+}
+
+function deSelectElement(element) {
+    element.classList.remove("selected");
+    const button = element.children[1];
     button.innerHTML = 'Select';
     button.style.width = '';
     button.style.padding = '';
 }
 
-function selectCourse(course) {
-    course.classList.add("selected");
-    const select_button = course.children[1];
+function selectElement(element) {
+    element.classList.add("selected");
+    const select_button = element.children[1];
     select_button.innerHTML = '<img src="images/icons/white-tick.svg" alt="tick">';
     select_button.style.width = 'auto';
     select_button.style.padding = '16px 8px';
 }
 
+function populateDetails(sourceElement, destination) {
+    // copy Html
+    const detailsElement = document.getElementById(destination + "-details");
+    detailsElement.innerHTML = sourceElement.innerHTML
+
+    //Sync any checkboxes that might be in the source element
+    const sourceCheckboxes = sourceElement.querySelectorAll('input[type="checkbox"]');
+    const destinationCheckboxes = detailsElement.querySelectorAll('input[type="checkbox"]');
+
+    destinationCheckboxes.forEach((checkbox, index) => {
+        checkbox.checked = sourceCheckboxes[index].checked;
+    });
+}
 
 function calculate_total() {
     // Get the value from the input (convert it to a number)
-    const cost = parseFloat(costInput.value);
+    const coursePerWeek = parseFloat(courseCost.value);
 
-    const dates = courseDateInput.value.split(" - ");
+    let dates = courseDateInput.value.split(" - ");
 
-    const start_date = new Date(dates[0])
-    const end_date = new Date(dates[1])
-    let length_of_study_weeks = (end_date.getDate() - start_date.getDate() + 3)/ 7
-    console.log(length_of_study_weeks)
+    let start_date = new Date(dates[0])
+    let end_date = new Date(dates[1])
+    const length_of_study_weeks = (end_date.getDate() - start_date.getDate() + 3)/ 7
 
+    const courseTotal = document.getElementById('course-total')
     // Check if the input is a valid number
-    if (!isNaN(length_of_study_weeks) && !isNaN(cost)) {
-        total.textContent = length_of_study_weeks * cost;
+    if (!isNaN(length_of_study_weeks) && !isNaN(coursePerWeek)) {
+        courseTotal.textContent = length_of_study_weeks * coursePerWeek;
     }
+
+    const accommodationPerWeek = parseFloat(accommodationCost.value);
+
+    dates = accommodationDateInput.value.split(" - ");
+    start_date = new Date(dates[0])
+    end_date = new Date(dates[1])
+    const length_of_stay_days = end_date.getDate() - start_date.getDate()
+
+    const accommodationTotal = document.getElementById('accommodation-total')
+    if (!isNaN(length_of_stay_days) && !isNaN(accommodationPerWeek)) {
+        accommodationTotal.textContent = length_of_stay_days * accommodationPerWeek;
+    }
+    
+    document.getElementById('total').textContent = parseFloat(accommodationTotal.textContent) + parseFloat(courseTotal.textContent) + parseFloat(document.getElementById('admission_fee').textContent);
 }
 
+// ensure exclusive checkboxes
+$(document).on('click', 'input[type="checkbox"].exclusive', function(event) {
+    if ($(this).prop('checked')) {
+        const checkboxGroup = $('input[type="checkbox"].exclusive').not($(this));
+        checkboxGroup.prop('checked', !$(this).prop('checked'));
+    }
+});
+
+// Link checkboxes in group
+$(document).on('click', 'input[type="checkbox"][data-group]', function(event) {
+    const checkboxGroup = $('input[type="checkbox"][data-group="' + $(this).data('group') + '"]');
+    checkboxGroup.prop('checked', $(this).prop('checked'));
+    syncAccommodationSelection(document.getElementById(accommodationInput.value))
+});
+
+const dateInput = document.getElementById('date');
+courseDateInput.value = dateInput.value
+accommodationDateInput.value = dateInput.value
 
 courseInput.addEventListener('valueChanged', (event) =>
     syncCourseSelection(document.getElementById(courseInput.value)));
-costInput.addEventListener('valueChanged', calculate_total);
+accommodationInput.addEventListener('valueChanged', (event) =>
+    syncAccommodationSelection(document.getElementById(accommodationInput.value)));
+
 courseDateInput.addEventListener('valueChanged', calculate_total);
+accommodationDateInput.addEventListener('valueChanged', calculate_total);
 
-
+// syncCourseSelection(document.getElementById(courseInput.value));
+// syncAccommodationSelection(document.getElementById(accommodationInput.value));
 calculate_total();
