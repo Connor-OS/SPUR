058d1110336589f6c6157f3d378f0f14997534f1
Connor <connor@NEWNB331.powerhub>
2025-05-17 11:50:18 +0100

    env setup and data import

---
+++ model/dataImport.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import fs from 'fs';
+import {City, School, schoolTypeEnum} from "/home/connor/Documents/SPUR/dist/model/dataModel.js";
+
+
+// MongoDB connection string (replace with your actual URI)
+const mongoURI = 'mongodb://localhost:27017/spur';
+
+
+async function uploadData() {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB');
+
+    // Read JSON file
+    const data = JSON.parse(fs.readFileSync('data/spur.cities.json', 'utf8'));
+
+    // Insert data into the collection
+    await City.insertMany(data);
+    console.log('Data uploaded successfully');
+
+    const school = JSON.parse(fs.readFileSync('data/spur.schools.json', 'utf8'));
+    const newcastle = await City.findOne({ name: 'Newcastle' });
+    school.city = newcastle._id;
+
+    const insertedSchool = await School.create(school);
+    console.log('School uploaded successfully:', insertedSchool);
+
+
+    // Disconnect
+    await mongoose.disconnect();
+    console.log('Disconnected from MongoDB');
+  } catch (error) {
+    console.error('Error uploading data:', error);
+  }
+}
+
+uploadData();
